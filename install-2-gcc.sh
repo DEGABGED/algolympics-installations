719e6bd32688d393112fb955cabb70da2eadad1d
asdfasdf <asdf@asdf.com>
2019-02-12 14:02:43 +0800

    Fix scripts 2 and 3

--- install-2-gcc.sh
+++ install-2-gcc.sh
@@ -4,8 +4,8 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install gcc-6 g++-6
 
+sudo update-alternatives --install "/usr/bin/gcc" "gcc" "/usr/bin/gcc-6" 1
 sudo update-alternatives --set gcc /usr/bin/gcc-6
+
+sudo update-alternatives --install "/usr/bin/g++" "g++" "/usr/bin/g++-6" 1
 sudo update-alternatives --set g++ /usr/bin/g++-6
-#sudo echo 'alias gcc="gcc-6"' >> ~/.bashrc
-# this needs gcc-6 to compile
-# USE ALIAS and put in .bashrc or .bash_profile to change, or change using PATH
--- install-3-codeblocks.sh
+++ install-3-codeblocks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir codeblocks
-tar -zxf codeblocks_17.12-1_amd64_stable.tar.xz -C codeblocks/
+tar -xf codeblocks_17.12-1_amd64_stable.tar.xz -C codeblocks/
 cd codeblocks/
 sudo dpkg -i *.deb

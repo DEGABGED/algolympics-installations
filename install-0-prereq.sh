af31270a22e7cd28cb78c7c26fc60b84759b408d
asdfasdf <asdf@asdf.com>
2019-02-12 18:56:58 +0800

    Use local neovim zip, Change order of prereq commands

--- install-0-prereq.sh
+++ install-0-prereq.sh
@@ -1,6 +1,6 @@
+sudoedit /etc/apt/apt.conf
+
 export http_proxy=
 export https_proxy=
 export HTTP_PROXY=
 export HTTPS_PROXY=
-
-sudoedit /etc/apt/apt.conf
--- install-7-neovim.sh
+++ install-7-neovim.sh
@@ -4,8 +4,8 @@ sudo apt-get install python-software-properties
 sudo apt-get install ninja-build libtool autoconf automake cmake g++ pkg-config unzip cmake-data
 # Build
 mkdir neovim
-tar -zxf neovim-0.3.4.zip -C neovim/
-cd neovim/
+unzip neovim-0.3.4.zip
+cd neovim-0.3.4/
 rm -r build/
 sudo make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
 sudo make install
